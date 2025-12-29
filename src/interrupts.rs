37cf8d0c3bf9e3803deda39eb2e5c6abbdc04311
Cheetah <DaverDiscord@gmail.com>
2025-12-28 21:16:50 -0800

    testy mctestface

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,9 @@
 use crate::println;
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::{
+    instructions,
+    structures::idt::{InterruptDescriptorTable, InterruptStackFrame},
+};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -16,4 +19,13 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+// -----
+// TESTS
+// -----
+
+#[test_case]
+fn test_breakpoint_exception() {
+    x86_64::instructions::interrupts::int3();
 }
